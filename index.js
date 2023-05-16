fb55a72c14abaefeade715526136e11472b0eded
Eugene Busygin <busygin1987@gmail.com>
2023-05-16 17:24:17 +0500

    fix

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ const botEditMsgReplyMarkup = async (bot, chatId, messageId) => {
     if (savedChats[chatId].gptType.type === constants.GPT_TYPE.imageStablediffusion.type) {
         choiceOptions = choiceTypeStablediffusionImageOptions;
     }
-    bot.editMessageReplyMarkup(emptyOptions, { chat_id: chatId, message_id: messageId });
+    bot.editMessageReplyMarkup(choiceOptions.reply_markup, { chat_id: chatId, message_id: messageId });
     // bot.editMessageReplyMarkup(chatId, messageId, null, choiceOptions);
     bot.sendMessage(chatId, `${savedChats[chatId].gptType.name} режим выбран, слушаю`);
 }
