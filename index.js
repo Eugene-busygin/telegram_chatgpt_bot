8cb3040b853252c7652f4bc8e051c6fe62511674
Eugene Busygin <busygin1987@gmail.com>
2023-06-08 08:50:25 +0500

    changed bot.launch

--- index.js
+++ index.js
@@ -959,13 +959,14 @@ const cb = function(req, res) {
 }
 
 try {
-    bot.launch({
-        webhook: {
-            domain: `${process.env.URL}`,
-            port: `${process.env.PORT}`,
-            cb
-        }
-    });
+    // bot.launch({
+    //     webhook: {
+    //         domain: `${process.env.URL}`,
+    //         port: `${process.env.PORT}`,
+    //         cb
+    //     }
+    // });
+    bot.launch();
     if (timerPingId) {
         timerPingId = null;
     }
